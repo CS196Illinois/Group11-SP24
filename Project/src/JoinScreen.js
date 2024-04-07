be27998f45aa73111318e40be320e8658e4531b6
PrestonSide <pres.watson@gmail.com>
2024-04-07 09:39:20 -0500

    Home Screen Tabs Created

---
+++ Project/src/JoinScreen.js
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native'
+import React from 'react'
+
+const JoinScreen = () => {
+    return (
+        <View>
+            <Text>Join a Group</Text>
+        </View>
+    )
+}
+
+export default JoinScreen
--- Project/src/navigations/Navigation.js
+++ Project/src/navigations/Navigation.js
@@ -1,17 +1,34 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import ProfileScreen from "../screens/ProfileScreen/ProfileScreen";
+import { createMaterialTopTabNavigator} from "@react-navigation/material-top-tabs"
 import HomeScreen from "../screens/HomeScreen";
-import MapScreen from "../screens/MapScreen"
+import MapScreen from "../screens/MapScreen";
+import CreateScreen from "../CreateScreen";
+import JoinScreen from "../JoinScreen";
 
+//Top Tabs
+const TopTabs = createMaterialTopTabNavigator();
+
+function TopTabsGroup() {
+    return (
+        <TopTabs.Navigator>
+            <TopTabs.Screen name ="Create" component={CreateScreen}/>
+            <TopTabs.Screen name ="Join" component={JoinScreen}/>
+        </TopTabs.Navigator>
+    )
+}
+
+
+//Bottom Tabs
 const Tab = createBottomTabNavigator();
 
 function TabGroup() {
     return (
         <Tab.Navigator>
-            <Tab.Screen name="Profile" component={ProfileScreen} />
-            <Tab.Screen name="Home" component={HomeScreen} />
             <Tab.Screen name="Map" component={MapScreen} />
+            <Tab.Screen name="Home" component={TopTabsGroup} />
+            <Tab.Screen name="Profile" component={ProfileScreen} />
         </Tab.Navigator>
     );
 }
